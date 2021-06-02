8da3de3cfb2e5fe91f05492ff06538653f207399
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-06-03 00:18:08 +0530

    Routing_in_flutter

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "./dummy_data.dart";
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = "/category-meals";
@@ -9,15 +10,17 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    // final categoryId = routeArgs['id'];
-
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
       body: ListView.builder(itemBuilder: (ctx,index){
-
-      },itemCount: ,),
+        return Text(categoryMeals[index].title);
+      },itemCount: categoryMeals.length,),
     );
   }
 }
