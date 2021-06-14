6f8072feaaffc34a73c96649a1324290398995e0
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-06-15 00:24:21 +0530

    origin

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "../widgets/main_drawer.dart";
 import './favourites_screen.dart';
 import './categories_screen.dart';
 
@@ -8,34 +9,50 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String,Object>> _pages = [
+    {
+      "page":CategoriesScreen(),
+      "title":"Categories"
+    },
+    {
+      "page":FavouritesScreen(),
+      "title":"Favourites"
+    }
+  ];
+  int _selectPageIndex = 0;
+  void _selectPage(int index){
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon:Icon(Icons.star,),
-                text: "Favourites",
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title']),
+      ),
+      drawer:MainDrawer(),
+      body: _pages[_selectPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            // ignore: deprecated_member_use
+            title: Text("Categories"),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavouritesScreen()
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            // ignore: deprecated_member_use
+            title: Text("Favourites"),
+          ),
+        ],
       ),
     );
   }
