6c69e276a9076135f45022cbe34570187924967c
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-06-15 16:03:59 +0530

    origin

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -83,6 +83,14 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: ElevatedButton(
+        child: Icon(
+          Icons.delete,
+        ),
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+      ),
     );
   }
 }
